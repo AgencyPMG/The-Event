b6010269a9d9bdd27dccd3816bd78ddaefca5790
Christopher Davis <cdavis9999@gmail.com>
2012-09-03 18:28:28 -0500

    A start

---
+++ the-event.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: The Event
+Plugin URI: https://github.com/AgencyPMG/The-Event
+Description: An events calendar without the front end.
+Version: 0.1
+Text Domain: the-event
+Domain Path: /lang
+Author: Christopher Davis
+Author URI: http://pmg.co/people/chris
+License: GPL2
+
+    Copyright 2012 Performance Media Group <seo@pmg.co>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+namespace PMG\TheEvent;
+
+define('PMG_TE_PATH', plugin_dir_path(__FILE__));
