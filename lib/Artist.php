d3c21b0dca497e83561bca01cd4a63b72b6dbb16
Christopher Davis <cdavis9999@gmail.com>
2012-09-04 02:15:44 -0500

    Artist type

--- lib/Artist.php
+++ lib/Artist.php
@@ -18,6 +18,39 @@ class Artist extends EventBase
 {
     public static function init()
     {
-        
+        add_action(
+            'init',
+            array(__CLASS__, 'register_type')
+        );
+    }
+
+    public static function register_type()
+    {
+        $labels = static::gen_type_labels(
+            _x('Artist', 'pmg_artist', 'the-event'),
+            _x('Artists', 'pmg_artist', 'the-event')
+        );
+        $labels['all_items'] = _x('Artists', 'pmg_artist', 'the-event');
+
+        $slug = self::opt('artist_slug', 'artist');
+        if(!$slug)
+            $slug = 'artist';
+
+        $args = array(
+            'labels'                => $labels,
+            'public'                => true,
+            'show_in_nav_menus'     => false,
+            'show_in_menu'          => 'edit.php?post_type=' . static::EVENT_TYPE,
+            'capability_type'       => 'page',
+            'supports'              => array('title', 'editor', 'thumbnail'),
+            'rewrite'               => array(
+                'slug'       => $slug,
+                'with_front' => false
+            )
+        );
+
+        $args = apply_filters('pmg_artist_type_args', $args);
+
+        register_post_type(static::ARTIST_TYPE, $args);
     }
 } // end class Artist
