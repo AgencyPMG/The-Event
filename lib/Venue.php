e2a025130c89b09fc2d218d428edad46621a7734
Christopher Davis <cdavis9999@gmail.com>
2012-09-04 02:12:38 -0500

    A venue type

--- lib/Venue.php
+++ lib/Venue.php
@@ -18,6 +18,39 @@ class Venue extends EventBase
 {
     public static function init()
     {
-        
+        add_action(
+            'init',
+            array(__CLASS__, 'register_type')
+        );
+    }
+
+    public static function register_type()
+    {
+        $labels = static::gen_type_labels(
+            _x('Venue', 'pmg_venue', 'the-event'),
+            _x('Venues', 'pmg_venue', 'the-event')
+        );
+        $labels['all_items'] = _x('Venues', 'pmg_venue', 'the-event');
+
+        $slug = self::opt('venue_slug', 'venue');
+        if(!$slug)
+            $slug = 'venue';
+
+        $args = array(
+            'labels'                => $labels,
+            'public'                => true,
+            'show_in_nav_menus'     => false,
+            'show_in_menu'          => 'edit.php?post_type=' . static::EVENT_TYPE,
+            'capability_type'       => 'page',
+            'supports'              => array('title', 'editor', 'thumbnail'),
+            'rewrite'               => array(
+                'slug'       => $slug,
+                'with_front' => false
+            )
+        );
+
+        $args = apply_filters('pmg_venue_type_args', $args);
+
+        register_post_type(static::VENUE_TYPE, $args);
     }
 } // end class Venue
